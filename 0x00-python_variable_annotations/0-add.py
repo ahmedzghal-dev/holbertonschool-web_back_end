a750a53b0f3fa009bd97e06e0f84372e376bb5c4
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-01-24 23:35:36 +0100

    0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,4 +5,7 @@ their sum as a float."""
 
 
 def add(a: float, b: float) -> float:
+    """function add that takes a float
+    a and a float b as arguments and
+    returns their sum as a float """
     return a + b
