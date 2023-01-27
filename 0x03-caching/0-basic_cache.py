50613181473d73d7c146f4818fd86072fe3272e8
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-01-27 22:05:59 +0100

    0

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-''' Create a class BasicCache that inherits from BaseCaching and is a caching system '''
+''' Create a class BasicCache that inherits from
+BaseCaching and is a caching system '''
 
 BaseCaching = __import__('base_caching').BaseCaching
 
