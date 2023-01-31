756742aa2932178d99707acbe6fc9eb3f0e3a1a2
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-01-31 19:17:32 +0100

    2

--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -49,16 +49,25 @@ class Server:
         return dataset[start:end]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Hypermedia pagination """
+        mydataset = self.dataset()
         data = self.get_page(page, page_size)
-        total_pages = math.ceil(len(self.dataset()) / page_size)
-        next_page = page + 1 if page < total_pages else None
-        prev_page = page - 1 if page > 1 else None
+        page_size = len(data)
+        total_pages = len(mydataset)
+        result = self.get_page(page + 1, page_size)
+        if result == []:
+            next_page = None
+        else:
+            next_page = page + 1
 
-        return {
-            "page_size": len(data),
-            "page": page,
-            "data": data,
-            "next_page": next_page,
-            "prev_page": prev_page,
-            "total_pages": total_pages
-        }
+        result = self.get_page(page - 1, page_size)
+        if result == []:
+            previous_page = None
+        else:
+            previous_page = page - 1
+
+        return {"page_size": page_size,
+                "page": page, "data": data,
+                "next_page": next_page,
+                "prev_page": previous_page,
+                "total_pages": total_pages}
