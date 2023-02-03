351b627ed144c9e5eabe280726220be0de310c16
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-02-03 23:22:48 +0100

    1

---
+++ 0x05-personal_data/encrypt_password.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" function that expects one string argument
+name password and returns a salted, hashed password"""
+
+import bcrypt
+
+
+def hash_password(password: str):
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
