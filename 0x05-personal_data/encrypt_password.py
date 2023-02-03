8827f047105e4a52329d0569262762903c61394e
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-02-03 23:23:30 +0100

    1

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -5,7 +5,7 @@ name password and returns a salted, hashed password"""
 import bcrypt
 
 
-def hash_password(password: str):
+def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
