805c90728303c747f5d18ec8b79a7a360af52270
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-02-03 23:17:58 +0100

    0

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -5,8 +5,9 @@ function called filter_datum that returns the log message obfuscated
 import re
 from typing import List
 import logging
-import os
+from os import environ
 import mysql.connector import connection
+from filtered_logger import get_logger, RedactingFormatter, PII_FIELDS
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
@@ -47,9 +48,32 @@ class RedactingFormatter(logging.Formatter):
         return logger
 
     def get_db() -> connection.MySQLConnection:
+        """connect to database"""
         host = os.environ.get("PERSONAL_DATA_DB_HOST", "localhost")
         user = os.environ.get("PERSONAL_DATA_DB_USERNAME", "root")
         password = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
         db_name = os.environ["PERSONAL_DATA_DB_NAME"]
         return mysql.connector.connect(host=host, user=user,
                                        password=password, database=db_name)
+
+    def main():
+        """main"""
+        logger = get_logger()
+        conn = get_db()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users")
+        rows = cursor.fetchall()
+        for row in rows:
+            log_line = RedactingFormatter.SEPARATOR.join(str(field)
+                                                         for field in row)
+            logger.info(RedactingFormatter().filter_datum(PII_FIELDS,
+                                                          RedactingFormatter.
+                                                          REDACTION,
+                                                          log_line,
+                                                          RedactingFormatter.
+                                                          SEPARATOR))
+            cursor.close()
+            conn.close()
+
+        if name == "main":
+            main()
