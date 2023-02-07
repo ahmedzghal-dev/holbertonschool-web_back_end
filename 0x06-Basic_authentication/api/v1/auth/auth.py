5378827ea49b13509cdb583c0cc5fef8d499b863
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-02-07 17:58:17 +0100

    auth

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""API authentication."""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """auth"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require auth"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """authorization header"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current user"""
+        return None
