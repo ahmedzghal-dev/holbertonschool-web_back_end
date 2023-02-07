4f6ac892866a78b23051cb9d89e830b919570bd8
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-02-07 18:33:33 +0100

    auth

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -16,7 +16,17 @@ class Auth():
         require auth function
         """
 
-        return False
+        if not path:
+            return True
+
+        if not excluded_paths or not excluded_paths:
+            return True
+
+        for excluded_path in excluded_paths:
+            if path == excluded_path or path.startswith(excluded_path + "/"):
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
