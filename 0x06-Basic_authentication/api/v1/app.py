b181bf1d28933efd1b1ae363d11b37784e3091e8
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-02-07 19:00:53 +0100

    app

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -46,24 +46,17 @@ def forbidden(error) -> str:
 @app.before_request
 def handler_before_request() -> None:
     """ the Flask method before_request"""
-    if auth in None:
+    if auth is None:
         return
-    if request.path in [
-                        '/api/v1/status/',
-                        '/api/v1/unauthorized/',
-                        '/api/v1/forbidden/']:
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/']
+    if not auth.require_auth(request.path, excluded_paths):
         return
-
-    if not auth.require_auth(request.path, [
-                                            '/api/v1/status/',
-                                            '/api/v1/unauthorized/',
-                                            '/api/v1/forbidden/']):
-        return
-
-    if not auth.authorization_header(request):
+    if auth.authorization_header(request) is None:
         abort(401)
-
-    if not auth.current_user(request):
+    if auth.current_user(request) is None:
         abort(403)
 
 
