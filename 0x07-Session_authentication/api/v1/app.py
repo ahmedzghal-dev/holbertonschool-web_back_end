0c510d0acf0afd2025278c153a65dab3bbcb7707
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-02-09 21:08:32 +0100

    app

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -53,7 +53,8 @@ def handle_before_request() -> None:
     excluded_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/']
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None:
