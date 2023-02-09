d09ba02568711d71a8c0ca89de6be939385ef69b
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-02-09 21:57:12 +0100

    session auth

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 """ Session Auth class """
 
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+
 
 class SessionAuth(Auth):
     """ My SessionAuth class """
