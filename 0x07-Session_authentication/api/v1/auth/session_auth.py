33680236e011c9aa70ad673e258bd839266f7bca
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-02-09 22:02:43 +0100

    session auth

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python3
 """ Session Auth class """
 
-from os import getenv
-from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-
+from api.v1.auth.auth import Auth
 
 class SessionAuth(Auth):
-    """ My SessionAuth class """
-    if os.environ.get('AUTH_IMPLEMENTATION') == 'SESSION':
-        auth = SessionAuth()
-    else:
-        auth = Auth()
+    pass
