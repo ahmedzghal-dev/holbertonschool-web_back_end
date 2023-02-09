9be9467884ab6db71dc6824a49a3f4e5ab4d451b
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-02-09 21:26:33 +0100

    app

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -59,9 +59,9 @@ def handle_before_request() -> None:
         return
     if auth.authorization_header(request) is None:
         abort(401)
+    request.current_user = auth.current_user(request)
     if request.current_user is None:
         abort(403)
-    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
