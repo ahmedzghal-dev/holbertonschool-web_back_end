47b2d2c8b744378d4f099cc7d3195b37bbe68993
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-02-09 21:55:53 +0100

    session auth

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 """ Session Auth class """
 
+
 class SessionAuth(Auth):
     """ My SessionAuth class """
+    if os.environ.get('AUTH_IMPLEMENTATION') == 'SESSION':
+        auth = SessionAuth()
+    else:
+        auth = Auth()
