c9652cb2c6a4c01ee56d62f3e64758e3344a0d35
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-02-09 21:22:16 +0100

    app

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -48,19 +48,19 @@ def handle_before_request():
     """
     Request validation!
     """
-    if auth:
+    if auth is not None:
         excluded_paths = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/',
             '/api/v1/auth_session/login/']
-        if not auth.require_auth(request.path, excluded_paths):
-            return
-        if auth.authorization_header(request) is None:
-            abort(401)
-        request.current_user = auth.current_user(request)
-        if request.current_user is None:
-            abort(403)
+        if auth.require_auth(request.path, api_list):
+            if (auth.authorization_header(request) is None and
+                    auth.session_cookie(request) is None):
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
+            request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
