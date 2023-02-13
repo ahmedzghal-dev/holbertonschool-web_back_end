20cd70576262561f905bc3ce2826fbf7cfc859ff
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-02-13 13:35:46 +0100

    app

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -9,20 +9,19 @@ from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-auth_type = getenv('AUTH_TYPE')
-if auth_type == 'auth':
-    from api.v1.auth.auth import Auth
+auth_type = getenv("AUTH_TYPE")
+if auth == 'auth':
     auth = Auth()
-if auth_type == 'basic_auth':
-    from api.v1.auth.basic_auth import BasicAuth
+elif auth_type == 'basic_auth':
     auth = BasicAuth()
-if auth_type == 'session_auth':
-    from api.v1.auth.session_auth import SessionAuth
+elif auth_type == 'session_auth':
     auth = SessionAuth()
 
 
@@ -35,36 +34,37 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized request handler
+    """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Forbidden request handler
+    """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
-def before_req():
+def handle_before_request() -> None:
     """
-    filters each request
+    Request validation!
     """
-    path_list = [
+    request.current_user = auth.current_user(request)
+    if auth is None:
+        return
+    excluded_paths = [
+        '/api/v1/auth_session/login/',
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/',
-        'api/v1/auth_session/login/'
-    ]
-    if auth.require_auth(request.path, path_list) and auth is not None:
-        if (auth.authorization_header(request)) is None\
-                and auth.session_cookie(request) is None:
+        '/api/v1/forbidden/']
+    if auth.require_auth(request.path, excluded_paths):
+        if not auth.authorization_header(
+                request) and not auth.session_cookie(request):
             abort(401)
-        if (auth.current_user(request)) is None:
+        if not auth.current_user(request):
             abort(403)
-        request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
