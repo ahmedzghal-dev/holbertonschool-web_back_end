1475103de2a94e2705eb9179034a7eb3edec1da3
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-02-16 18:55:39 +0100

    db

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -32,10 +32,6 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user to the database
-        with the given email and hashed_password.
-
-        Returns:
-            User: The newly created User object.
         """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
