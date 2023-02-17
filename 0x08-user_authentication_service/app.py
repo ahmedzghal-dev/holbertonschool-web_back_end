c460b40b87b4f4f9ad5f654f386be4510e16068e
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-02-17 23:09:39 +0100

    app

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -25,8 +25,8 @@ def users():
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
-@app.route('/sessions', methods=['POST'])
-def login():
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
     """Login method"""
     email = request.form.get('email')
     password = request.form.get('password')
