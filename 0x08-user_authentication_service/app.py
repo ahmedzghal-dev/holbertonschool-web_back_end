bef983ec320af19464f090d798279ca87e579dfc
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-02-17 23:24:01 +0100

    app

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """FLASK module"""
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 app = Flask(__name__)
