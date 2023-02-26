7ec033d2f60de7d5a99fcf98c0d2e0220cb53d37
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-02-26 19:32:13 +0100

    102

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+This file contains various functions with type hints.
+Functions perform different operations on input data and return result.
+All functions have been annotated with type hints.
+"""
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """_summary_
+    Args:
+        lst (tuple): _description_
+        factor (int, optional): _description_. Defaults to 2.
+    Returns:
+        list: _description_
+    """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(tuple(array), 2)
+
+zoom_3x = zoom_array(tuple(array), 3)
