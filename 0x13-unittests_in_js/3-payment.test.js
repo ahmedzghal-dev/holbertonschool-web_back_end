5de465d49c7e8bac6a6eee41dd2266a82ecd5d0f
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-05-29 03:10:33 +0100

    3

---
+++ 0x13-unittests_in_js/3-payment.test.js
@@ -0,0 +1,12 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+const { assert } = require('chai');
+
+describe("sendPaymentRequestToApi", function () {
+    sinon.spy(Utils, "calculateNumber");
+    Utils.calculateNumber('SUM', 100, 20);
+
+    let spyCall = Utils.calculateNumber.getCall(0);
+    assert.equal(spyCall.returnValue, sendPaymentRequestToApi(100, 20));
+});
